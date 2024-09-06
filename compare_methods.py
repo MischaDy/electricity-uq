a163c3fee0a7ed90c9eada1e3b22f98883802d3b
Mischa <MischaDy@users.noreply.github.com>
2024-09-06 18:35:07 +0200

    reformatting (black)

--- compare_methods.py
+++ compare_methods.py
@@ -7,7 +7,10 @@ from sklearn.ensemble import RandomForestRegressor
 from uncertainty_toolbox import get_all_accuracy_metrics
 
 from helpers import get_data
-from mapie_plot_ts_tutorial import train_base_model, estimate_pred_interals_no_pfit_enbpi
+from mapie_plot_ts_tutorial import (
+    train_base_model,
+    estimate_pred_interals_no_pfit_enbpi,
+)
 from quantile_regression import estimate_quantiles
 
 # todo: potential for bugs!
@@ -15,19 +18,30 @@ N_POINTS_TEMP = 100  # per group
 
 
 def main():
-    print('loading data')
-    X_train, X_test, y_train, y_test, X, y = get_data(N_POINTS_TEMP, return_full_data=True)
-    print('data shapes:', X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+    print("loading data")
+    X_train, X_test, y_train, y_test, X, y = get_data(
+        N_POINTS_TEMP, return_full_data=True
+    )
+    print("data shapes:", X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
     # print('plotting data')
     # plot_data(X_train, X_test, y_train, y_test)
 
-    print('training base model')
-    model_params_choices = {"max_depth": randint(2, 30), "n_estimators": randint(10, 100)}
-    model = train_base_model(RandomForestRegressor, model_params_choices=model_params_choices, X_train=X_train,
-                             y_train=y_train, load_trained_model=True, cv_n_iter=10)
+    print("training base model")
+    model_params_choices = {
+        "max_depth": randint(2, 30),
+        "n_estimators": randint(10, 100),
+    }
+    model = train_base_model(
+        RandomForestRegressor,
+        model_params_choices=model_params_choices,
+        X_train=X_train,
+        y_train=y_train,
+        load_trained_model=True,
+        cv_n_iter=10,
+    )
 
-    print('estimating CP intervals')
+    print("estimating CP intervals")
     random_state = 59
     alpha = 0.05
     cv_mapie_ts = BlockBootstrap(
@@ -37,26 +51,27 @@ def main():
         model, cv_mapie_ts, alpha, X_test, X_train, y_train, skip_base_training=True
     )
 
-    print('estimating QR intervals')
+    print("estimating QR intervals")
     y_pred_qr, y_pis_qr = estimate_quantiles(X_train, y_train, X_test, ci_alpha=0.1)
 
-    print('plotting CP vs QR')
+    print("plotting CP vs QR")
     y_preds_both = [y_pred_enbpi_no_pfit, y_pred_qr]
     y_pis_both = [y_pis_enbpi_no_pfit, y_pis_qr]
-    labels_both = ['CP', 'QR']
-    plot_intervals(X, y, X_train, y_train, X_test, y_test, y_preds_both, y_pis_both, labels_both)
+    labels_both = ["CP", "QR"]
+    plot_intervals(
+        X, y, X_train, y_train, X_test, y_test, y_preds_both, y_pis_both, labels_both
+    )
 
-    print('computing metrics')
+    print("computing metrics")
 
-    print('========= CP =========\n')
+    print("========= CP =========\n")
     y_test_arr = y_test.to_numpy().squeeze()
     acc_metrics_cp = get_all_accuracy_metrics(y_pred_enbpi_no_pfit, y_test_arr)
     print(acc_metrics_cp)
 
-    print('========= QR =========\n')
+    print("========= QR =========\n")
     acc_metrics_qr = get_all_accuracy_metrics(y_pred_qr, y_test_arr)
     print(acc_metrics_qr)
-
 
     # acc_cp, acc_qr = ..., ...  # todo
     # print('accuracy CP vs QR:', acc_cp, acc_qr)
@@ -65,8 +80,8 @@ def main():
     # print('CRPS CP vs QR:', crps_cp, crps_qr)
 
 
-
 # PLOTTING
+
 
 def plot_data(X_train, X_test, y_train, y_test):
     """visualize training and test sets"""
@@ -84,7 +99,9 @@ def plot_data(X_train, X_test, y_train, y_test):
     plt.show()
 
 
-def plot_intervals(X, y, X_train, y_train, X_test, y_test, y_preds_both, y_pis_both, labels_both):
+def plot_intervals(
+    X, y, X_train, y_train, X_test, y_test, y_preds_both, y_pis_both, labels_both
+):
     num_train_steps = X_train.shape[0]
     num_test_steps = X_test.shape[0]
     num_steps_total = num_train_steps + num_test_steps
@@ -97,7 +114,9 @@ def plot_intervals(X, y, X_train, y_train, X_test, y_test, y_preds_both, y_pis_b
     fig, axs = plt.subplots(
         nrows=2, ncols=1, figsize=(14, 8), sharey="row", sharex="col"
     )
-    for i, (ax, y_pred, y_pis, label) in enumerate(zip(axs, y_preds_both, y_pis_both, labels_both)):
+    for i, (ax, y_pred, y_pis, label) in enumerate(
+        zip(axs, y_preds_both, y_pis_both, labels_both)
+    ):
         ax.plot(x_plot_full, y, color="black", linestyle="dashed", label="True mean")
 
         ax.scatter(
@@ -109,12 +128,12 @@ def plot_intervals(X, y, X_train, y_train, X_test, y_test, y_preds_both, y_pis_b
             label="training points",
         )
 
-        ax.plot(x_plot_test, y_pred, label=f"median prediction {label}", color='green')
+        ax.plot(x_plot_test, y_pred, label=f"median prediction {label}", color="green")
         ax.fill_between(
             x_plot_test.ravel(),
             y_pis[:, 0],
             y_pis[:, 1],
-            color='green',
+            color="green",
             alpha=0.2,
             label=rf"{label} 95% confidence interval",
         )
@@ -126,5 +145,5 @@ def plot_intervals(X, y, X_train, y_train, X_test, y_test, y_preds_both, y_pis_b
     plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
