defbcf83feccb99031a4549241476a66fba974a0
Mischa <MischaDy@users.noreply.github.com>
2024-09-21 22:08:15 +0200

    add minor function

--- helpers.py
+++ helpers.py
@@ -7,33 +7,41 @@ from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 
 
-def get_data(n_points_temp, filepath='data.pkl', input_cols=None, output_cols=None, return_full_data=False):
+def get_data(
+    n_points_temp,
+    filepath="data.pkl",
+    input_cols=None,
+    output_cols=None,
+    return_full_data=False,
+):
     """load and prepare data"""
     if input_cols is None:
         input_cols = [
-            'load_last_week',
-            'load_last_hour',
-            'load_now',
-            'is_workday',
-            'is_saturday_and_not_holiday',
-            'is_sunday_or_holiday',
-            'is_heating_period',
+            "load_last_week",
+            "load_last_hour",
+            "load_now",
+            "is_workday",
+            "is_saturday_and_not_holiday",
+            "is_sunday_or_holiday",
+            "is_heating_period",
         ]
     if output_cols is None:
-        output_cols = ['load_next_hour']
+        output_cols = ["load_next_hour"]
     df = pd.read_pickle(filepath)
 
     mid = df.shape[0] // 2
-    X = df[input_cols].iloc[mid - n_points_temp: mid + n_points_temp]
-    y = df[output_cols].iloc[mid - n_points_temp: mid + n_points_temp]
+    X = df[input_cols].iloc[mid - n_points_temp : mid + n_points_temp]
+    y = df[output_cols].iloc[mid - n_points_temp : mid + n_points_temp]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, shuffle=False)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.5, shuffle=False
+    )
     if return_full_data:
         return X_train, X_test, y_train, y_test, X, y
     return X_train, X_test, y_train, y_test
 
 
-def plot_data(X_train, X_test, y_train, y_test, filename='data', do_save_figure=False):
+def plot_data(X_train, X_test, y_train, y_test, filename="data", do_save_figure=False):
     """visualize training and test sets"""
     num_train_steps = X_train.shape[0]
     num_test_steps = X_test.shape[0]
@@ -47,7 +55,7 @@ def plot_data(X_train, X_test, y_train, y_test, filename='data', do_save_figure=
     plt.ylabel("energy data (details TODO)")
     plt.legend(["Training data", "Test data"])
     if do_save_figure:
-        IO_HELPER.save_plot(f'{filename}_{N_POINTS_TEMP}.png')
+        IO_HELPER.save_plot(f"{filename}_{N_POINTS_TEMP}.png")
     plt.show()
 
 
@@ -56,7 +64,13 @@ def unzip(iterable):
 
 
 class IOHelper:
-    def __init__(self, base_folder, arrays_folder='arrays', models_folder='models', plots_folder='plots'):
+    def __init__(
+        self,
+        base_folder,
+        arrays_folder="arrays",
+        models_folder="models",
+        plots_folder="plots",
+    ):
         # (self.arrays_folder,
         #  self.models_folder,
         #  self.plots_folder) = (os.path.join(base_folder, folder_name)
@@ -66,8 +80,10 @@ class IOHelper:
         self.plots_folder = os.path.join(base_folder, plots_folder)
         self.folders = [self.arrays_folder, self.models_folder, self.plots_folder]
         for folder in self.folders:
-            try: os.mkdir(folder)
-            except FileExistsError: pass
+            try:
+                os.mkdir(folder)
+            except FileExistsError:
+                pass
 
     def get_array_savepath(self, filename):
         return os.path.join(self.arrays_folder, filename)
@@ -82,13 +98,26 @@ class IOHelper:
         return np.load(self.get_array_savepath(filename))
 
     def load_model(self, filename):
-        return pickle.load(open(self.get_model_savepath(filename), 'rb'))
+        return pickle.load(open(self.get_model_savepath(filename), "rb"))
 
     def save_array(self, array, filename):
         np.save(self.get_array_savepath(filename), array)
 
     def save_model(self, model, filename):
-        pickle.dump(model, open(self.get_model_savepath(filename), 'wb'))
+        pickle.dump(model, open(self.get_model_savepath(filename), "wb"))
 
     def save_plot(self, filename):
         plt.savefig(self.get_plot_savepath(filename))
+
+
+def check_is_ordered(pred, pis):
+    lower_ordered = np.all(pis[:, 0] > pred)
+    higher_ordered = np.all(pred <= pis[:, 1])
+    if not lower_ordered:
+        print("not lower-ordered!")
+    if not higher_ordered:
+        print("not higher-ordered!")
+    if lower_ordered and higher_ordered:
+        print("ordered.")
+        return True
+    return False
