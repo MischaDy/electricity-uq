0faa4c148307371078e23199edca93354ba60992
Mischa <MischaDy@users.noreply.github.com>
2024-10-15 12:27:30 +0200

    finish adding laplace - unstable

--- my_compare_methods_script.py
+++ my_compare_methods_script.py
@@ -4,6 +4,7 @@ from mapie.subsample import BlockBootstrap
 from scipy.stats import randint
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_pinball_loss
+from torch.utils.data import TensorDataset, DataLoader
 from uncertainty_toolbox.metrics_scoring_rule import crps_gaussian, nll_gaussian
 
 from compare_methods import UQ_Comparer
@@ -16,7 +17,6 @@ from conformal_prediction import (
 from quantile_regression import estimate_quantiles as estimate_quantiles_qr
 
 import torch
-from laplace_helper.util import plot_regression
 
 from laplace import Laplace
 
@@ -44,7 +44,7 @@ class My_UQ_Comparer(UQ_Comparer):
     @staticmethod
     def _mean_pinball_loss(y_true, y_quantiles, quantiles):
         """
-    
+
         :param y_true:
         :param y_quantiles: array of shape (n_samples, n_quantiles)
         :param quantiles:
@@ -53,7 +53,7 @@ class My_UQ_Comparer(UQ_Comparer):
         return np.mean([mean_pinball_loss(y_true, y_quantiles[:, ind], alpha=quantile)
                         for ind, quantile in enumerate(quantiles)])
 
-    def train_base_model(self, X_train, y_train, model_params_choices=None):
+    def train_base_model_normal(self, X_train, y_train, model_params_choices=None):
         # todo: more flexibility in choosing (multiple) base models
         if model_params_choices is None:
             model_params_choices = {
@@ -68,6 +68,37 @@ class My_UQ_Comparer(UQ_Comparer):
             load_trained_model=True,
             cv_n_iter=10,
         )
+
+    def train_base_model(
+        self,
+        X_train: pd.DataFrame,
+        y_train: pd.DataFrame,
+        model_params_choices=None,
+        n_epochs=100,
+        batch_size=1,
+        random_state=711,
+    ):
+        # todo: more flexibility in choosing (multiple) base models
+        torch.manual_seed(random_state)
+
+        model = torch.nn.Sequential(
+            torch.nn.Linear(1, 50), torch.nn.Tanh(), torch.nn.Linear(50, 1)
+        )
+
+        # todo: consistent input expectations!
+        X_train, y_train = map(torch.Tensor, (X_train, y_train))
+        train_dataset = TensorDataset(X_train, y_train)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size)
+
+        criterion = torch.nn.MSELoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+        for i in range(n_epochs):
+            for X, y in train_loader:
+                optimizer.zero_grad()
+                loss = criterion(model(X), y)
+                loss.backward()
+                optimizer.step()
+        return model
 
     def posthoc_conformal_prediction(
         self, X_train, y_train, X_test, quantiles, model, random_state=42
@@ -89,28 +120,25 @@ class My_UQ_Comparer(UQ_Comparer):
         y_quantiles = None  # todo: sth with y_pis
         return y_pred, y_quantiles, y_std
 
-    def posthoc_laplace(self, random_state=711):
+    def posthoc_laplace(
+        self,
+        X_train,
+        y_train,
+        X_test,
+        quantiles,
+        model,
+        n_epochs=100,
+        batch_size=1,
+        random_state=711,
+    ):
         # todo: offer option to alternatively optimize parameters and hyperparameters of the prior jointly (cf. example
         #  script)?
+        train_dataset = TensorDataset(X_train, y_train)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size)
 
-        n_epochs = 1000
-        torch.manual_seed(random_state)
-
-        model = torch.nn.Sequential(
-            torch.nn.Linear(1, 50), torch.nn.Tanh(), torch.nn.Linear(50, 1)
-        )
-
-        # train MAP
-        criterion = torch.nn.MSELoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
-        for i in range(n_epochs):
-            for X, y in train_loader:
-                optimizer.zero_grad()
-                loss = criterion(model(X), y)
-                loss.backward()
-                optimizer.step()
-
-        la = Laplace(model, "regression", subset_of_weights="all", hessian_structure="full")
+        la = Laplace(
+            model, "regression"
+        )  # , subset_of_weights="all", hessian_structure="full")
         la.fit(train_loader)
         log_prior, log_sigma = (
             torch.ones(1, requires_grad=True),
@@ -123,37 +151,38 @@ class My_UQ_Comparer(UQ_Comparer):
             neg_marglik.backward()
             hyper_optimizer.step()
 
-        # Serialization for fitted quantities
-        state_dict = la.state_dict()
-        torch.save(state_dict, "state_dict.bin")
+        # # Serialization for fitted quantities
+        # state_dict = la.state_dict()
+        # torch.save(state_dict, "state_dict.bin")
+        #
+        # la = Laplace(model, "regression", subset_of_weights="all", hessian_structure="full")
+        # # Load serialized, fitted quantities
+        # la.load_state_dict(torch.load("state_dict.bin"))
 
-        la = Laplace(model, "regression", subset_of_weights="all", hessian_structure="full")
-        # Load serialized, fitted quantities
-        la.load_state_dict(torch.load("state_dict.bin"))
-
-        x = X_test.flatten().cpu().numpy()
+        # x = X_test.flatten().cpu().numpy()
 
         # Two options:
         # 1.) Marginal predictive distribution N(f_map(x_i), var(x_i))
         # The mean is (m,k), the var is (m,k,k)
         f_mu, f_var = la(X_test)
 
-        # 2.) Joint pred. dist. N((f_map(x_1),...,f_map(x_m)), Cov(f(x_1),...,f(x_m)))
-        # The mean is (m*k,) where k is the output dim. The cov is (m*k,m*k)
-        f_mu_joint, f_cov = la(X_test, joint=True)
+        # # 2.) Joint pred. dist. N((f_map(x_1),...,f_map(x_m)), Cov(f(x_1),...,f(x_m)))
+        # # The mean is (m*k,) where k is the output dim. The cov is (m*k,m*k)
+        # f_mu_joint, f_cov = la(X_test, joint=True)
 
-        # Both should be true
-        assert torch.allclose(f_mu.flatten(), f_mu_joint)
-        assert torch.allclose(f_var.flatten(), f_cov.diag())
+        # # Both should be true
+        # assert torch.allclose(f_mu.flatten(), f_mu_joint)
+        # assert torch.allclose(f_var.flatten(), f_cov.diag())
 
         f_mu = f_mu.squeeze().detach().cpu().numpy()
         f_sigma = f_var.squeeze().detach().sqrt().cpu().numpy()
         pred_std = np.sqrt(f_sigma**2 + la.sigma_noise.item() ** 2)
 
-        plot_regression(
-            X_train, y_train, x, f_mu, pred_std, file_name="regression_example", plot=True, file_path='.'
-        )
+        # plot_regression(
+        #     X_train, y_train, x, f_mu, pred_std, file_name="regression_example", plot=True, file_path='.'
+        # )
 
+        y_pred, y_quantiles, y_std = f_mu, None, pred_std
 
         # model = get_model()
         # la, model, margliks, losses = marglik_training(
@@ -185,7 +214,7 @@ class My_UQ_Comparer(UQ_Comparer):
         #     file_name="regression_example_online",
         #     plot=False,
         # )
-
+        return y_pred, y_quantiles, y_std
 
     def native_quantile_regression(self, X_train, y_train, X_test, quantiles):
         y_pred, y_quantiles = estimate_quantiles_qr(X_train, y_train, X_test, alpha=quantiles)
