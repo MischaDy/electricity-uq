44e9878d85453f5407d713db381c658f1b934b57
Mikhail Dubovoy <MischaDy@users.noreply.github.com>
2024-12-12 02:18:53 +0100

    run QR remotely!

--- my_compare_methods_script.py
+++ my_compare_methods_script.py
@@ -27,7 +27,7 @@ from metrics import rmse, smape_scaled, crps, nll_gaussian, mean_pinball_loss
 METHOD_WHITELIST = [
 #    "posthoc_conformal_prediction",
 #    "posthoc_laplace",
-#    "native_quantile_regression",
+    "native_quantile_regression",
 #    "native_gp",
 #    "native_mvnn",
 ]
