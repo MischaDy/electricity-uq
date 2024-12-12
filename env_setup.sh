a173b63e04d0c6a3803a17c5a8c7ac1c0e67332c
Mischa <MischaDy@users.noreply.github.com>
2024-12-12 01:14:03 +0100

    update gitignore and move unneeded files to hidden folder

--- env_setup.sh
+++
@@ -1,5 +0,0 @@
-conda create -y -n masterarbeit
-conda activate masterarbeit
-conda config --append channels conda-forge
-conda install -y --file .\requirements.txt
-pip install git+https://git@github.com/wiseodd/asdl@asdfghjkl
