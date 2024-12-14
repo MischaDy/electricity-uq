c5dd5162677a42ca5fc574162ea5d9fb34bbd290
Mischa <MischaDy@users.noreply.github.com>
2024-12-14 15:11:59 +0100

    update env setup script (keops bugfix)

--- env_setup.sh
+++ env_setup.sh
@@ -3,3 +3,5 @@ conda activate masterarbeit
 conda config --append channels conda-forge
 conda install -y --file .\requirements.txt
 pip install git+https://git@github.com/wiseodd/asdl@asdfghjkl
+xpip install git+https://github.com/cornellius-gp/gpytorch.git@fe4619b546f0bc41f801b9b5ac57c424c25bd3fe
+xpip install git+https://github.com/aladagemre/django-notification.git@2927346f4c513a217ac8ad076e494dd1adbf70e1
