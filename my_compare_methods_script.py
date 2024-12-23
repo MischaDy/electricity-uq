a92f1ef178f944fe9c61c78923f6fe90595bb38e
Mischa <MischaDy@users.noreply.github.com>
2024-12-23 18:27:04 +0100

    linreg test run

--- my_compare_methods_script.py
+++ my_compare_methods_script.py
@@ -22,7 +22,7 @@ STANDARDIZE_DATA = True
 PLOT_DATA = False
 PLOT_UQ_RESULTS = True
 PLOT_BASE_RESULTS = True
-SHOW_PLOTS = False
+SHOW_PLOTS = True
 SAVE_PLOTS = True
 SKIP_BASE_MODEL_COPY = True
 
@@ -33,14 +33,14 @@ STORAGE_PATH = "comparison_storage"
 
 
 METHOD_WHITELIST = [
-     "posthoc_conformal_prediction",
-     "posthoc_laplace",
-     "native_quantile_regression",
-     "native_gpytorch",
-     "native_mvnn",
+     # "posthoc_conformal_prediction",
+     # "posthoc_laplace",
+     # "native_quantile_regression",
+     # "native_gpytorch",
+     # "native_mvnn",
      'base_model_linreg',
-     'base_model_rf',
-     'base_model_nn',
+     # 'base_model_rf',
+     # 'base_model_nn',
 ]
 POSTHOC_BASE_BLACKLIST = {
     'posthoc_laplace': {
@@ -87,7 +87,7 @@ METHODS_KWARGS = {
         'save_model': True,
     },
     "base_model_linreg": {
-        "skip_training": False,
+        "skip_training": True,
         "n_jobs": -1,
         "save_model": True,
     },
