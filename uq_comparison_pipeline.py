46b785de52ff8ec33dfb6c32bc5dcc10e43928cd
Mischa <MischaDy@users.noreply.github.com>
2024-12-25 00:17:45 +0100

    start debugging gpytorch

--- uq_comparison_pipeline.py
+++ uq_comparison_pipeline.py
@@ -22,7 +22,7 @@ STANDARDIZE_DATA = True
 PLOT_DATA = False
 PLOT_UQ_RESULTS = True
 PLOT_BASE_RESULTS = True
-SHOW_PLOTS = False
+SHOW_PLOTS = True
 SAVE_PLOTS = True
 SKIP_BASE_MODEL_COPY = True
 SHOULD_SAVE_RESULTS = True
@@ -33,14 +33,14 @@ SKIP_TRAINING_ALL = False
 STORAGE_PATH = "comparison_storage"
 
 METHOD_WHITELIST = [
-    "posthoc_conformal_prediction",
-    "posthoc_laplace",
-    "native_quantile_regression",
+    # "posthoc_conformal_prediction",
+    # "posthoc_laplace",
+    # "native_quantile_regression",
     "native_gpytorch",
-    "native_mvnn",
-    'base_model_linreg',
-    'base_model_rf',
-    'base_model_nn',
+    # "native_mvnn",
+    # 'base_model_linreg',
+    # 'base_model_rf',
+    # 'base_model_nn',
 ]
 POSTHOC_BASE_BLACKLIST = {
     'posthoc_laplace': {
@@ -70,12 +70,12 @@ METHODS_KWARGS = {
     },
     "native_gpytorch": {
         'skip_training': False,
-        'n_iter': 100,
+        'n_iter': 200,
         'val_frac': 0.1,
-        'lr': 1e-2,
+        'lr': 1e-3,
         'show_progress': True,
         'show_plots': True,
-        'do_plot_losses': False,
+        'do_plot_losses': True,
         'save_model': True,
     },
     "posthoc_conformal_prediction": {
