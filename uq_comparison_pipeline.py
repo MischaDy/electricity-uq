650d3d75665656c68cad3abfcddc87792673d753
Mischa <MischaDy@users.noreply.github.com>
2024-12-31 01:45:21 +0100

    test run QR only. still strange train/test loss ordering

--- uq_comparison_pipeline.py
+++ uq_comparison_pipeline.py
@@ -37,9 +37,9 @@ STORAGE_PATH = "comparison_storage"
 METHOD_WHITELIST = [
     # "posthoc_conformal_prediction",
     # "posthoc_laplace_approximation",
-    # "native_quantile_regression_nn",
+    "native_quantile_regression_nn",
     # "native_gpytorch",
-    "native_mvnn",
+    # "native_mvnn",
     # 'base_model_linreg',
     # 'base_model_rf',
     # 'base_model_nn',
