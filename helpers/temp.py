6169ab05ef8588181925a89f09ed4fd626838f3c
Mischa <MischaDy@users.noreply.github.com>
2025-01-03 19:58:21 +0100

    add temp debug script

---
+++ helpers/temp.py
@@ -0,0 +1,13 @@
+import pickle
+
+
+def get_model():
+    filepath = r'comparison_storage\\models\\base_model_nn_n35136_it300_nh2_hs50.pth'
+    with open(filepath, "rb") as file:
+        model = pickle.load(file)
+    print('success!')
+    return model
+
+
+if __name__ == '__main__':
+    get_model()
