cb1ac86601a7effe04de60eec72605b091ed1c41
Mischa <MischaDy@users.noreply.github.com>
2025-01-04 16:05:18 +0100

    improve resetting dimensions in model wrapper: figure out original dim once, then always reset to it

--- helpers/model_wrapper.py
+++ helpers/model_wrapper.py
@@ -1,26 +1,30 @@
-from typing import Literal
-
 from helpers import misc_helpers
 
 
 class ModelWrapper:
-    def __init__(self, model, output_dim: Literal[1, 2] = 2):
+    def __init__(self, model, output_dim: int = 2):
+        """
+
+        :param model:
+        :param output_dim: must be either 1 or 2
+        """
+        assert output_dim in {1, 2}
         self.model_ = model
         self.output_dim = output_dim
-        self._output_dim_old = None
+        self.output_dim_orig = output_dim
 
     def predict(self, input_):
         output = self.model_.predict(input_)
         output = misc_helpers.make_arr_1d(output) if self.output_dim == 1 else misc_helpers.make_arr_2d(output)
         return output
 
-    def set_output_dim(self, output_dim):
-        self._output_dim_old = self.output_dim
+    def set_output_dim(self, output_dim, orig=False):
         self.output_dim = output_dim
+        if orig:
+            self.output_dim_orig = output_dim
 
     def reset_output_dim(self):
-        # switch values around
-        self._output_dim_old, self.output_dim = self.output_dim, self._output_dim_old
+        self.output_dim = self.output_dim_orig
 
     def __call__(self, input_):
         return self.predict(input_)
