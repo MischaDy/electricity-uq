0802ff93725c8003bb3b96bba2292f401db492da
Mischa <MischaDy@users.noreply.github.com>
2025-01-04 21:00:27 +0100

    update deploy config (shorter runtime)

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=2         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=0-02:00            # Runtime in D-HH:MM
+#SBATCH --time=0-00:45            # Runtime in D-HH:MM
 #SBATCH --partition=2080-galvani  # Partition to submit to
 #SBATCH --gres=gpu:2              # optionally type and number of gpus
 #SBATCH --mem=4G                 # Memory pool for all cores (see also --mem-per-cpu)
