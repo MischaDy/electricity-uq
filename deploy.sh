fcb3aa14f58eb51ac2ffefd5d0e4839425b05942
Mischa <MischaDy@users.noreply.github.com>
2025-01-04 18:03:33 +0100

    update deploy config: more time and GPU, less mem (to detect errors)

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=2         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=0-01:00            # Runtime in D-HH:MM
+#SBATCH --time=0-02:00            # Runtime in D-HH:MM
 #SBATCH --partition=2080-galvani  # Partition to submit to
-#SBATCH --gres=gpu:1              # optionally type and number of gpus
-#SBATCH --mem=5G                 # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --gres=gpu:2              # optionally type and number of gpus
+#SBATCH --mem=4G                 # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=logs/job_%j.out  # File to which STDOUT will be written
 #SBATCH --error=logs/job_%j.err   # File to which STDERR will be written
 #SBATCH --mail-type=END,FAIL           # Type of email notification- BEGIN,END,FAIL,ALL
