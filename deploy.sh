8e7d1bf5567026679f7b4ee0a867eb8280f784c9
Mikhail Dubovoy <MischaDy@users.noreply.github.com>
2025-01-05 01:08:06 +0100

    update deploy config: dont notify on start, reduce runtime

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=2         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=0-00:30            # Runtime in D-HH:MM
+#SBATCH --time=0-00:20            # Runtime in D-HH:MM
 #SBATCH --partition=2080-galvani  # Partition to submit to
 #SBATCH --gres=gpu:2              # optionally type and number of gpus
 #SBATCH --mem=5G                  # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=logs/job_%j.out  # File to which STDOUT will be written
 #SBATCH --error=logs/job_%j.err   # File to which STDERR will be written
-#SBATCH --mail-type=BEGIN,END,FAIL           # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-type=END,FAIL      # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=mikhail.dubovoy@student.uni-tuebingen.de  # Email to which notifications will be sent
 
 # print info about current job
