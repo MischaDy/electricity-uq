bb168258e3850f90dd45817e115181e0ef60510e
Mischa <MischaDy@users.noreply.github.com>
2025-01-07 00:29:24 +0100

    increase max runtime

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=4         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=0-03:00            # Runtime in D-HH:MM
+#SBATCH --time=0-05:00            # Runtime in D-HH:MM
 #SBATCH --partition=2080-galvani  # Partition to submit to
 #SBATCH --gres=gpu:8              # optionally type and number of gpus
 #SBATCH --mem=8G                  # Memory pool for all cores (see also --mem-per-cpu)
