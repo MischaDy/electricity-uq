1278b0bb1860fb917db942b36f89100a067b19c8
Mikhail Dubovoy <MischaDy@users.noreply.github.com>
2025-01-06 23:15:07 +0100

    update output file names (native)

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@
 #SBATCH --partition=2080-galvani  # Partition to submit to
 #SBATCH --gres=gpu:8              # optionally type and number of gpus
 #SBATCH --mem=12G                 # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH --output=logs/job.out     # File to which STDOUT will be written
-#SBATCH --error=logs/job.err      # File to which STDERR will be written
+#SBATCH --output=logs/job_native.out     # File to which STDOUT will be written
+#SBATCH --error=logs/job_native.err      # File to which STDERR will be written
 #SBATCH --mail-type=END,FAIL      # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=mikhail.dubovoy@student.uni-tuebingen.de  # Email to which notifications will be sent
 
