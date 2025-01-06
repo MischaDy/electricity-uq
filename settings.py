dd727bb39c90db648cb2b9191d9245c06a05484e
Mikhail Dubovoy <MischaDy@users.noreply.github.com>
2025-01-06 23:16:31 +0100

    update settings to run native methods only

--- settings.py
+++ settings.py
@@ -43,12 +43,12 @@ LOGGING_LEVEL = logging.INFO
 STORAGE_PATH = "comparison_storage"
 
 METHOD_WHITELIST = [
-    'base_model_linreg',
-    'base_model_nn',
-    'base_model_hgbr',
-    # 'native_gpytorch',
-    # 'native_mvnn',
-    # 'native_quantile_regression_nn',
+#    'base_model_linreg',
+#    'base_model_nn',
+#    'base_model_hgbr',
+     'native_gpytorch',
+     'native_mvnn',
+     'native_quantile_regression_nn',
     # 'posthoc_conformal_prediction',
     # 'posthoc_laplace_approximation',
 ]
