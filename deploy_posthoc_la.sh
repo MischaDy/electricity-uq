75da9dba3435b9f134ca08b2e6c410c5cd165c94
Mikhail Dubovoy <MischaDy@users.noreply.github.com>
2025-01-07 03:16:49 +0100

    update LA config

--- deploy_posthoc_la.sh
+++ deploy_posthoc_la.sh
@@ -2,10 +2,10 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=4         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=0-07:00            # Runtime in D-HH:MM
+#SBATCH --time=0-00:30            # Runtime in D-HH:MM
 #SBATCH --partition=2080-galvani  # Partition to submit to
-#SBATCH --gres=gpu:8              # optionally type and number of gpus
-#SBATCH --mem=8G                 # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --gres=gpu:4              # optionally type and number of gpus
+#SBATCH --mem=4G                 # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=logs/job_posthoc_la.out     # File to which STDOUT will be written
 #SBATCH --error=logs/job_posthoc_la.err      # File to which STDERR will be written
 #SBATCH --mail-type=END,FAIL      # Type of email notification- BEGIN,END,FAIL,ALL
