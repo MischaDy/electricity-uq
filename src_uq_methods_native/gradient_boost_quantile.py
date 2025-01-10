254480d298ea747dc3da459d4d6c7766e49a4ddc
Mischa <MischaDy@users.noreply.github.com>
2025-01-10 15:31:59 +0100

    many temp changes: ignore CV, try much higher LR, and quite high max iter - much better!

--- src_uq_methods_native/gradient_boost_quantile.py
+++ src_uq_methods_native/gradient_boost_quantile.py
@@ -13,7 +13,7 @@ import numpy as np
 from helpers.io_helper import IO_Helper
 
 
-IO_HELPER = IO_Helper('comparison_storage')
+IO_HELPER = IO_Helper('qhgbr_storage')
 
 
 class HGBR_Quantile:
@@ -66,17 +66,19 @@ class HGBR_Quantile:
                 'min_samples_leaf': stats.randint(15, 100),
                 'l2_regularization': [0, 1e-4, 1e-3, 1e-2, 1e-1],
             }
-        cv_maker = partial(
-            RandomizedSearchCV,
-            param_distributions=model_param_distributions,
-            n_iter=cv_n_iter,
-            cv=TimeSeriesSplit(n_splits=cv_n_splits),
-            scoring="neg_root_mean_squared_error",
-            random_state=random_seed,
-            verbose=verbose,
-            n_jobs=n_jobs,
-        )
-        cv_objs = {quantile: cv_maker(estimator=model) for quantile, model in self.models.items()}
+        # cv_maker = partial(
+        #     RandomizedSearchCV,
+        #     param_distributions=model_param_distributions,
+        #     n_iter=cv_n_iter,
+        #     cv=TimeSeriesSplit(n_splits=cv_n_splits),
+        #     scoring="neg_root_mean_squared_error",
+        #     random_state=random_seed,
+        #     verbose=verbose,
+        #     n_jobs=n_jobs,
+        # )
+        # cv_objs = {quantile: cv_maker(estimator=model) for quantile, model in self.models.items()}
+        cv_objs = self.models  # temp
+
         y_train = y_train.ravel()
 
         # todo: parallelize!
@@ -84,7 +86,7 @@ class HGBR_Quantile:
             logging.info(f'fitting cv obj {i}/{len(cv_objs)}...')
             cv_obj.fit(X_train, y_train)
             logging.info(f'done.')
-        self.models = {quantile: cv_obj.best_estimator_ for quantile, cv_obj in cv_objs.items()}
+        # self.models = {quantile: cv_obj.best_estimator_ for quantile, cv_obj in cv_objs.items()}  # temp
 
     def predict(self, X_pred, as_dict=True):
         # todo: parallelize?
@@ -128,6 +130,11 @@ def train_hgbr_quantile(
         val_frac=val_frac,
         n_iter_no_change=n_iter_no_change,
         random_seed=random_seed,
+        max_iter=1000,
+        lr=0.2,
+        # max_leaf_nodes=0,
+        # min_samples_leaf=0,
+        l2_regularization=1e-3,
     )
     X_train, y_train = misc_helpers.add_val_to_train(X_train, X_val, y_train, y_val)
     model.fit(X_train, y_train, cv_n_iter=cv_n_iter, cv_n_splits=cv_n_splits, n_jobs=n_jobs, random_seed=random_seed,
@@ -192,17 +199,17 @@ def test_qhgbr():
 
     quantiles = [0.05, 0.10, 0.50, 0.90, 0.95]  # settings.QUANTILES
 
-    cv_n_iter = 5
+    cv_n_iter = 0
     cv_n_splits = 3
     verbose = 2
     n_iter_no_change = 30
 
     model_param_distributions = {
         # 'max_features': stats.randint(1, X_train.shape[1]),
-        "max_iter": stats.randint(10, 1000),
-        'learning_rate': stats.loguniform(0.015, 0.15),
-        'max_leaf_nodes': stats.randint(10, 100),
-        'min_samples_leaf': stats.randint(15, 100),
+        "max_iter": [1000], # stats.randint(10, 1000),
+        'learning_rate': [1e-3],  # stats.loguniform(0.015, 0.15),
+        'max_leaf_nodes': [50], # stats.randint(10, 100),
+        'min_samples_leaf': [2], # stats.randint(15, 100),
         'l2_regularization': [0, 1e-4, 1e-3, 1e-2, 1e-1],
     }
 
@@ -253,7 +260,7 @@ def test_qhgbr():
         n_jobs=-1,
         verbose=verbose,
         val_frac=val_frac,
-        n_iter_no_change=n_iter_no_change,
+        # n_iter_no_change=n_iter_no_change,
     )
     prefix = 'qhgbr'
     postfix = f'n{n_samples}_it{cv_n_iter}'
