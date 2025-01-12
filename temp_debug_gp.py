save preds, bugfix pred scaling, major refactoring

temp_debug_gp.py
@@ -12,16 +12,21 @@ from helpers import misc_helpers
 RUN_SIZE = 'full'
 SHOW_PLOT = True
 SAVE_PLOT = True
+SAVE_PREDS = True
 N_SAMPLES_TO_PLOT = 1600
 
 
 def main():
-    X_test, X_train, y_test, y_train = get_data()
+    X_test, X_train, y_test, y_train, scaler_y = get_data()
     likelihood, model = load_models(X_train)
-    interval = 90
-    quantiles = settings.QUANTILES
-    plot_train(X_train, interval, likelihood, model, quantiles, y_train, n_samples_to_plot=N_SAMPLES_TO_PLOT)
-    plot_test(X_test, interval, likelihood, model, quantiles, y_test, n_samples_to_plot=N_SAMPLES_TO_PLOT)
+
+    for X_pred, y_true, is_training_data in [(X_train, y_train, True), (X_test, y_test, False)]:
+        X_pred = X_pred[:N_SAMPLES_TO_PLOT]
+        y_pred, y_quantiles = predict(X_pred, model, likelihood, settings.QUANTILES, is_training_data=is_training_data,
+                                      save_preds=SAVE_PREDS)
+        y_pred, y_quantiles = misc_helpers.inverse_transform_ys(scaler_y, y_pred, y_quantiles)
+        plot(y_true, y_pred, y_quantiles, interval=90, is_training_data=is_training_data,
+             n_samples_to_plot=N_SAMPLES_TO_PLOT)
 
 
 def load_models(
@@ -46,25 +51,20 @@ def get_data():
     # noinspection PyProtectedMember
     X_train, y_train, X_val, y_val, X_test, y_test, X, y, scaler_y = misc_helpers._quick_load_data(RUN_SIZE)
     X_train, y_train, X_val, y_val, _ = prepare_data(X_train, y_train, X_val, y_val, np.ones((1, 1)))
-    return X_test, X_train, y_test, y_train
+    return X_test, X_train, y_test, y_train, scaler_y
 
 
-def plot_train(X_train, interval, likelihood, model, quantiles, y_train, n_samples_to_plot=1600):
-    X_pred = X_train[:n_samples_to_plot]
-    y_pred_train, y_quantiles_train, _ = predict_with_gpytorch(model, likelihood, X_pred, quantiles)
-    np.save('temp_y_pred_train_gp.npy', y_pred_train)
-    np.save('temp_y_quantiles_train_gp.npy', y_quantiles_train)
-    plot_uq_single_dataset(y_train, y_pred_train, y_quantiles_train, uq_method='gp', is_training_data=True,
-                           interval=interval, show_plot=SHOW_PLOT, save_plot=SAVE_PLOT,
-                           n_samples_to_plot=n_samples_to_plot)
+def predict(X_pred, model, likelihood, quantiles, is_training_data, save_preds=True):
+    y_pred, y_quantiles, _ = predict_with_gpytorch(model, likelihood, X_pred, quantiles)
+    if save_preds:
+        infix = 'train' if is_training_data else 'test'
+        np.save(f'temp_y_pred_{infix}_gp.npy', y_pred)
+        np.save(f'temp_y_quantiles_{infix}_gp.npy', y_quantiles)
+    return y_pred, y_quantiles
 
 
-def plot_test(X_test, interval, likelihood, model, quantiles, y_test, n_samples_to_plot=1600):
-    X_pred = X_test[:n_samples_to_plot]
-    y_pred_test, y_quantiles_test, _ = predict_with_gpytorch(model, likelihood, X_pred, quantiles)
-    np.save('temp_y_pred_test_gp.npy', y_pred_test)
-    np.save('temp_y_quantiles_test_gp.npy', y_quantiles_test)
-    plot_uq_single_dataset(y_test, y_pred_test, y_quantiles_test, uq_method='gp', is_training_data=True,
+def plot(y_true, y_pred, y_quantiles, interval, is_training_data, n_samples_to_plot=1600):
+    plot_uq_single_dataset(y_true, y_pred, y_quantiles, uq_method='gp', is_training_data=is_training_data,
                            interval=interval, show_plot=SHOW_PLOT, save_plot=SAVE_PLOT,
                            n_samples_to_plot=n_samples_to_plot)
 