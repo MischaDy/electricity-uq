7fc09ec3b539d907a3dcbe2af38150bc8852adb4
Mischa <MischaDy@users.noreply.github.com>
2025-01-14 00:23:05 +0100

    add logging; add config; use MAE; major refactoring new configs: show/save plots, io_helper dirs, which methods to load

--- add_metrics_plots.py
+++ add_metrics_plots.py
@@ -1,46 +1,129 @@
+import logging
+from functools import partial
+
 import seaborn as sns
 from matplotlib import pyplot as plt
 
+from helpers.io_helper import IO_Helper
 from helpers.uq_arr_helpers import get_uq_method_to_arrs_gen
 
 
+logging.basicConfig(level=logging.INFO, force=True)
+
+
 RUN_SIZE = 'small'
+SHOW_PLOTS = False
+SAVE_PLOTS = True
+SMALL_IO_HELPER = True
+UQ_METHODS_WHITELIST = {
+    # 'qhgbr',
+    # 'qr',
+    # 'gp',
+    # 'mvnn',
+    'cp_hgbr',
+    # 'cp_linreg',
+    # 'cp_nn',
+    # 'la_nn',
+}
+UQ_METHOD_TO_ARR_NAMES_DICT = {
+    'qhgbr': [
+        'native_qhgbr_y_pred_n210432_it0.npy',
+        'native_qhgbr_y_quantiles_n210432_it0.npy',
+        'native_qhgbr_y_std_n210432_it0.npy',
+    ],
+    'qr': [
+        'native_quantile_regression_nn_y_pred_n210432_it300_nh2_hs50.npy',
+        'native_quantile_regression_nn_y_quantiles_n210432_it300_nh2_hs50.npy',
+        'native_quantile_regression_nn_y_std_n210432_it300_nh2_hs50.npy',
+    ],
+    'gp': [
+        'native_gpytorch_y_pred_n210432_it200.npy',
+        'native_gpytorch_y_quantiles_n210432_it200.npy',
+        'native_gpytorch_y_std_n210432_it200.npy',
+    ],
+    'mvnn': [
+        'native_mvnn_y_pred_n210432_it100_nh2_hs50.npy',
+        'native_mvnn_y_quantiles_n210432_it100_nh2_hs50.npy',
+        'native_mvnn_y_std_n210432_it100_nh2_hs50.npy',
+    ],
+    'cp_hgbr': [
+        'posthoc_conformal_prediction_base_model_hgbr_y_pred_n640_it5.npy',
+        'posthoc_conformal_prediction_base_model_hgbr_y_quantiles_n640_it5.npy',
+        'posthoc_conformal_prediction_base_model_hgbr_y_std_n640_it5.npy',
+    ],
+    'cp_linreg': [
+        'posthoc_conformal_prediction_base_model_linreg_y_pred_n210432_it5.npy',
+        'posthoc_conformal_prediction_base_model_linreg_y_quantiles_n210432_it5.npy',
+        'posthoc_conformal_prediction_base_model_linreg_y_std_n210432_it5.npy',
+    ],
+    'cp_nn': [
+        'posthoc_conformal_prediction_base_model_nn_y_pred_n210432_it5.npy',
+        'posthoc_conformal_prediction_base_model_nn_y_quantiles_n210432_it5.npy',
+        'posthoc_conformal_prediction_base_model_nn_y_std_n210432_it5.npy',
+    ],
+    'la_nn': [
+        'posthoc_laplace_approximation_base_model_nn_y_pred_n210432_it100.npy',
+        'posthoc_laplace_approximation_base_model_nn_y_quantiles_n210432_it100.npy',
+        'posthoc_laplace_approximation_base_model_nn_y_std_n210432_it100.npy',
+    ],
+}
 
 
 def main():
-    from helpers.misc_helpers import _quick_load_data
-    from helpers.io_helper import IO_Helper
     from helpers import misc_helpers
+    from helpers._metrics import crps, mae
 
-    X_train, y_train, X_val, y_val, X_test, y_test, X, y, scaler_y = _quick_load_data(RUN_SIZE)
+    X_train, y_train, X_val, y_val, X_test, y_test, X, y, scaler_y = misc_helpers._quick_load_data(RUN_SIZE)
     X_train, y_train = misc_helpers.add_val_to_train(X_train, X_val, y_train, y_val)
     n_samples_train = X_train.shape[0]
 
-    io_helper_small = IO_Helper(arrays_folder='arrays_small', models_folder='models_small')
-    uq_method_to_arrs_gen = get_uq_method_to_arrs_gen(io_helper=io_helper_small)
+    io_helper_kwargs = {'arrays_folder': 'arrays_small', 'models_folder': 'models_small'} if SMALL_IO_HELPER else {}
+    io_helper = IO_Helper(**io_helper_kwargs)
+    uq_method_to_arrs_gen = get_uq_method_to_arrs_gen(
+        uq_method_to_arr_names_dict=UQ_METHOD_TO_ARR_NAMES_DICT,
+        uq_methods_whitelist=UQ_METHODS_WHITELIST,
+        io_helper=io_helper,
+    )
     for uq_method, uq_arrs in uq_method_to_arrs_gen:
         arrs_train, arrs_test = split_pred_arrs_train_test(uq_arrs, n_samples_train=n_samples_train)
         for y_true, arrs, are_train_arrs in [(y_train, arrs_train, True), (y_test, arrs_test, False)]:
             y_pred, y_quantiles, y_std = arrs
-            plot_hist_crps(y_true, y_quantiles)
+            error_scores_dict = {
+                'crps': partial(crps, y_true, y_quantiles, keep_dim=True),
+                'ae': partial(mae, y_true, y_pred, keep_dim=True),
+            }
+            for error_score_name, error_func in error_scores_dict.items():
+                logging.info(f'computing {error_score_name}')
+                error_arr = error_func()
+
+                logging.info('saving array')
+                filename = _get_filename(infix=error_score_name, uq_method=uq_method, are_train_arrs=are_train_arrs)
+                io_helper.save_array(error_arr, filename=filename)
+
+                logging.info('plotting histogram')
+                sns.displot(error_arr, bins=25, stat='density')
+                if SAVE_PLOTS:
+                    io_helper.save_plot(filename=filename)
+                if SHOW_PLOTS:
+                    plt.show(block=True)
+                plt.close()
+
+
+def _get_filename(infix: str, uq_method: str, are_train_arrs: bool, ext: str = None):
+    filename_y_pred = UQ_METHOD_TO_ARR_NAMES_DICT[uq_method][0]
+    assert 'y_pred' in filename_y_pred
+    base_filename = filename_y_pred.split('_y_pred_')[0]
+    dataset = 'training' if are_train_arrs else 'test'
+    filename = f'{base_filename}_{infix}_{dataset}'
+    if ext is not None:
+        filename = f'{filename}.{ext}'
+    return filename
 
 
 def split_pred_arrs_train_test(arrs, n_samples_train):
     arrs_train = list(map(lambda arr: arr[:n_samples_train], arrs))
     arrs_test = list(map(lambda arr: arr[n_samples_train:], arrs))
     return arrs_train, arrs_test
-
-
-def plot_hist_crps(y_true, y_quantiles, bins=25, stat='density'):
-    from helpers._metrics import crps
-    score = crps(y_true, y_quantiles, keep_dim=True)
-    sns.displot(score, bins=bins, stat=stat)
-    # todo: add labels
-    _show()
-
-
-def _show():
-    plt.show(block=True)
 
 
 if __name__ == '__main__':
