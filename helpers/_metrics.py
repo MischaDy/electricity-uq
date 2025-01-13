c5eb2809717364f8f66489b6c36e76c725933cdc
Mischa <MischaDy@users.noreply.github.com>
2025-01-13 17:38:17 +0100

    add option to keep sample dimension in all UQ metrics

--- helpers/_metrics.py
+++ helpers/_metrics.py
@@ -21,9 +21,10 @@ def mse(y_true: np.ndarray, y_pred: np.ndarray):
     return np.mean((y_pred - y_true) ** 2)
 
 
-def mean_pinball_loss(y_true: np.ndarray, y_quantiles: np.ndarray, quantiles: np.ndarray):
+def mean_pinball_loss(y_true: np.ndarray, y_quantiles: np.ndarray, quantiles: np.ndarray, keep_dim=False):
     """
 
+    :param keep_dim:
     :param y_true:
     :param y_quantiles: array of shape (n_samples, n_quantiles)
     :param quantiles:
@@ -32,25 +33,32 @@ def mean_pinball_loss(y_true: np.ndarray, y_quantiles: np.ndarray, quantiles: np
     from sklearn.metrics import mean_pinball_loss as mean_pinball_loss_
     if y_quantiles is None:
         return None
-    return np.mean([
+    loss = [
         mean_pinball_loss_(y_true, y_quantiles[:, ind], alpha=quantile)
         for ind, quantile in enumerate(quantiles)
-    ])
+    ]
+    return loss if keep_dim else np.mean(loss)
 
 
-def nll_gaussian(y_true: np.ndarray, y_pred: np.ndarray, y_std: np.ndarray):
-    from uncertainty_toolbox.metrics_scoring_rule import nll_gaussian as nll_gaussian_
+def nll_gaussian(y_true: np.ndarray, y_pred: np.ndarray, y_std: np.ndarray, keep_dim=False):
+    # based on uncertainty_toolbox.metrics_scoring_rule.nll_gaussian
     if y_std is None:
         return None
-    return nll_gaussian_(y_pred, y_std, y_true)
+
+    from scipy import stats
+
+    residuals = y_pred - y_true
+    nll = -1 * stats.norm.logpdf(residuals, scale=y_std)
+    return nll if keep_dim else np.mean(nll)
 
 
-def crps(y_true: np.ndarray, y_quantiles: np.ndarray):
+def crps(y_true: np.ndarray, y_quantiles: np.ndarray, keep_dim=False):
     """
     We interpret y_quantiles as a "deterministic sample" from the underlying predicted distribution. Hence, we use
     method='fair'. See the docs for details:
     https://scores.readthedocs.io/en/stable/api.html#scores.probability.crps_for_ensemble
 
+    :param keep_dim:
     :param y_true:
     :param y_quantiles:
     :return:
@@ -62,7 +70,9 @@ def crps(y_true: np.ndarray, y_quantiles: np.ndarray):
     sample_dim, ensemble_member_dim = 'samples', 'sample_quantiles'
     y_true = xarray.DataArray(y_true, dims=[sample_dim])
     y_quantiles = xarray.DataArray(y_quantiles, dims=[sample_dim, ensemble_member_dim])
+
+    preserve_dims = [sample_dim] if keep_dim else None
     crps = scores.probability.crps_for_ensemble(
-        y_quantiles, y_true, ensemble_member_dim=ensemble_member_dim, method='fair'
+        y_quantiles, y_true, ensemble_member_dim=ensemble_member_dim, method='fair', preserve_dims=preserve_dims,
     )
     return crps.to_numpy()
