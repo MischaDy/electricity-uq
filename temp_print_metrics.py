7bc7a670c11e0e73c829f2096e624f8117266846
Mischa <MischaDy@users.noreply.github.com>
2025-01-17 02:07:51 +0100

    add temp metrics printing file

---
+++ temp_print_metrics.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+metrics_path = './comparison_storage/metrics/'
+metrics_file_names = os.listdir(metrics_path)
+
+for metrics_file_name in metrics_file_names:
+    metrics_file_path = os.path.join(metrics_path, metrics_file_name)
+    with open(metrics_file_path, 'r') as file:
+        metrics = json.load(file)
+    print(metrics_file_name)
+    for key, value in metrics.items():
+        print(f'{key}: {round(value, 6)}')
+    print()
+
+
