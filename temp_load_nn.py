a230b3136b776c71859b8edf8703e203f62221e7
Mischa <MischaDy@users.noreply.github.com>
2025-01-17 19:28:58 +0100

    update temp load nn script

--- temp_load_nn.py
+++ temp_load_nn.py
@@ -10,12 +10,12 @@ dim_in = X_train.shape[1]
 model_kwargs = {
     'dim_in': dim_in,
     'train_size_orig': 210432,
-    "n_iter": 30,
+    "n_iter": 2,
     "num_hidden_layers": 2,
     "hidden_layer_size": 50,
     'activation': torch.nn.LeakyReLU,  # defaults to leaky ReLU
     "weight_decay": 1e-3,
-    "lr": 1e-3,  # defaults to 1e-2 if use_scheduler is true
+    "lr": 1e-5,  # defaults to 1e-2 if use_scheduler is true
     'use_scheduler': True,
     "lr_patience": 10,
     "lr_reduction_factor": 0.8,
@@ -28,6 +28,6 @@ model_kwargs = {
     'early_stop_patience': 30,
 }
 io_helper = IO_Helper()
-model2 = io_helper.load_torch_model_statedict(model_class=NN_Estimator, model_kwargs=model_kwargs,
-                                              filename='base_model_nn_n210432_it200_nh2_hs50_dict.pth')
-io_helper.save_torch_model(model2, filename='base_model_nn_n210432_it200_nh2_hs50_model2.pth')
+model = io_helper.load_torch_model_statedict(model_class=NN_Estimator, model_kwargs=model_kwargs,
+                                             filename='base_model_nn_n210432_it300_nh2_hs50.pth')
+# io_helper.save_torch_model(model, filename='base_model_nn_n210432_it200_nh2_hs50_model2.pth')
