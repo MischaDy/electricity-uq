95d77f4575a63cbc2706ceebc5ace26bcfc6f072
Mischa <MischaDy@users.noreply.github.com>
2025-01-22 20:59:31 +0100

    found bug!

--- temp_compute_metrics.py
+++ temp_compute_metrics.py
@@ -23,23 +23,23 @@ METRICS_WHITELIST_DET = set([
 METRICS_WHITELIST_UQ = set([
     # "crps",
     # "nll_gaussian",
-    # "mean_pinball",
+    "mean_pinball",
     # "ssr",
-    "coverage",
+    # "coverage",
 ])
 
 METHODS_WHITELIST = set([
-    'base_model_hgbr',
-    'base_model_linreg',
-    'base_model_nn',
-    'native_qhgbr',
-    'native_qr',
-    'native_gp',
-    'native_mvnn',
+    # 'base_model_hgbr',
+    # 'base_model_linreg',
+    # 'base_model_nn',
+    # 'native_qhgbr',
+    # 'native_qr',
+    # 'native_gp',
+    # 'native_mvnn',
     'posthoc_cp_hgbr',
-    'posthoc_cp_linreg',
-    'posthoc_cp_nn',
-    'posthoc_la_nn',
+    # 'posthoc_cp_linreg',
+    # 'posthoc_cp_nn',
+    # 'posthoc_la_nn',
 ])
 UQ_METHOD_TO_ARR_NAMES_DICT = {
     'base_model_hgbr': ['base_model_hgbr_n210432_it30_its3.npy'],
