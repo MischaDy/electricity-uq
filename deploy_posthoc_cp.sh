55c3fd24fc1337c887c173186269991926c5684f
Mischa <MischaDy@users.noreply.github.com>
2025-01-25 01:13:14 +0100

    upgrade deploy config

--- deploy_posthoc_cp.sh
+++ deploy_posthoc_cp.sh
@@ -2,19 +2,19 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=2         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=0-01:00            # Runtime in D-HH:MM
+#SBATCH --time=0-02:00            # Runtime in D-HH:MM
 #SBATCH --partition=2080-galvani  # Partition to submit to
-#SBATCH --gres=gpu:1              # optionally type and number of gpus
-#SBATCH --mem=6G                 # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --gres=gpu:2              # optionally type and number of gpus
+#SBATCH --mem=7G                 # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=logs/job_%j.out     # File to which STDOUT will be written
 #SBATCH --error=logs/job_%j.err      # File to which STDERR will be written
 #SBATCH --mail-type=END,FAIL      # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=mikhail.dubovoy@student.uni-tuebingen.de  # Email to which notifications will be sent
 
 # print info about current job
-#echo "---------- JOB INFOS ------------"
-#scontrol show job $SLURM_JOB_ID
-#echo -e "---------------------------------\n"
+echo "---------- JOB INFOS ------------"
+scontrol show job $SLURM_JOB_ID
+echo -e "---------------------------------\n"
 
 # Due to a potential bug, we need to manually load our bash configurations first
 source "$HOME"/.bashrc
